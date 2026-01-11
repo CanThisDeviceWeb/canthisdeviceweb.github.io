f63a7b5ae8440927d0688b60e976725e022eb386
Ben Klumski <b64964357@gmail.com>
2026-01-11 12:40:09 +0100

    added python fixes

--- Python.py
+++ Python.py
@@ -1,2 +1,9 @@
+import sys
+import os
+import platform
 
-html_content = "<html><body><h2>Python is working!</h2></body></html>"
+python_version = sys.version
+os_name = os.name
+platform_name = platform.platform()
+
+html_content = f"<h2>Python is working!</h2><p>Python Version: {python_version}</p><p>OS Name: {os_name}</p><p>Platform Name: {platform_name}</p>"
