62c72eec543a864405c1d9c7c4ff54baede963cd
Ben Klumski <b64964357@gmail.com>
2026-01-14 19:48:55 +0100

    new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-380p.webm 	new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-720p.webm 	new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-[k99bMtg4zRk]-1080p-Full-HD.webm 	modified:   CSS.html 	new file:   PHP.PHP 	modified:   React.jsx 	modified:   TSReact.tsx 	modified:   index.html 	modified:   js.js 	new file:   style.css 	modified:   CSS.html

--- js.js
+++ js.js
@@ -1,4 +1,2 @@
 alert("JavaScript Is WORKING!")
 console.log("JavaScript Is WORKING!")
-document.body.style.backgroundColor = "#fbff05ff"
-document.body.innerHTML += "<h2>JavaScript is working!</h2>"
