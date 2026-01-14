9ae893c007b46d6e6eaa3d85062b9f7faa6dead8
Ben Klumski <b64964357@gmail.com>
2026-01-14 19:48:55 +0100

    new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-380p.webm 	new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-720p.webm 	new file:   CRISPR-Cas9-Mr.-Sandman-Parody-A-Capella-Science-[k99bMtg4zRk]-1080p-Full-HD.webm 	new file:   PHP.PHP 	modified:   React.jsx 	modified:   TSReact.tsx 	modified:   index.html 	modified:   js.js

--- React.jsx
+++ React.jsx
@@ -1,5 +1,3 @@
 alert("JavaScript React should work!");
 exception = "JavaScript React isn't working!";
 console.log("If you see this message, JavaScript React is working correctly.");
-document.body.style.backgroundColor = "#00d1ecff";
-document.body.innerHTML += "<h2>JavaScript React is working!</h2>";
--- TSReact.tsx
+++ TSReact.tsx
@@ -1,4 +1,2 @@
 alert("TypeScript React should work!");
 console.log("if you see this message, TypeScript React is Working!")
-document.body.style.backgroundColor = "#003a86ff";
-document.body.innerHTML += "<h2>TypeScript React is working!</h2>";
